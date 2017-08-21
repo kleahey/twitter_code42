83b98cabbe745179d45ca2066e64c3f2bed73493
Kevin Leahey <kleahey@me.com>
2017-08-21 16:10:47 -0400

    Twitter timeline spec created

---
+++ spec/features/twitter_timeline_spec.rb
@@ -0,0 +1,16 @@
+# When a user visits the home page,
+# ensure at least two tweets get displayed on the screen
+
+require 'feature_helper'
+
+RSpec.feature 'twitter timeline' do
+  scenario 'a user views a twitter timeline' do
+    visit root_path
+
+    within '[data-twitter-app]' do
+      within '[data-tweets]' do
+        expect(page).to have_css '.tweet', minimum: 2
+      end
+    end
+  end
+end
